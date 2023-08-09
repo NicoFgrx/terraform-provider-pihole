2165de9b76df69a591dd0bad917738b88142bc23
Nicolas FAUGEROUX <nicolas.faugeroux@protonmail.com>
2023-08-09 19:03:19 +0200

    add docker_compose for example

--- examples/resources/dnsrecord/resource.tf
+++ examples/resources/dnsrecord/resource.tf
@@ -7,9 +7,17 @@ terraform {
 }
 
 # search env variables
-provider "pihole" {}
+provider "pihole" {
+  url = "http://localhost:8080/admin/api.php"
+  token = "96cf46f9e9312ea9ad00f5f9e63b25643f701246357068549a6c2ea3d163bf1e"
+}
 
-resource "pihole_dnsrecord" "box" {
-  domain = "box.pasfastoche.lan"
-  ip     = "192.168.1.1"
+resource "pihole_dnsrecord" "example-1" {
+  domain = "test1.example.com"
+  ip     = "1.1.1.1"
+}
+
+resource "pihole_dnsrecord" "example-2" {
+  domain = "test2.example.com"
+  ip     = "2.2.2.2"
 }
