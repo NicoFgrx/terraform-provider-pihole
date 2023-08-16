eff9a387a89043722e01a2e56967bda1fd661cfc
Nicolas FAUGEROUX <nicolas.faugeroux@protonmail.com>
2023-08-16 23:10:53 +0200

    remove resource pihole_dnsrecord in cname example folder

--- examples/resources/cname/resource.tf
+++ examples/resources/cname/resource.tf
@@ -12,13 +12,8 @@ provider "pihole" {
   token = "96cf46f9e9312ea9ad00f5f9e63b25643f701246357068549a6c2ea3d163bf1e"
 }
 
-resource "pihole_dnsrecord" "example-1" {
-  domain = "test.example.com"
-  ip     = "1.1.1.1"
-}
-
 resource "pihole_cname" "example-2" {
     domain = "test"
-    target = "test.example.com"  
+    target = "test1.example.com"  
 }
 
